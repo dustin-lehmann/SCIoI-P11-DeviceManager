74268e2e8b877ffbd45a22069400617efbf276e8
s81674 <koberrobotics@web.de>
2024-06-03 23:31:53 +0200

    added second ws for the two way com

--- scioi_hardware_manager/hardware_manager/main.py
+++ scioi_hardware_manager/hardware_manager/main.py
@@ -1,24 +1,41 @@
 import time
 
 from hardware_manager.manager import HardwareManager
+from hardware_manager.helper.websocket_server import WebsocketClass
+global ws_stream
+global ws_messages
 
 
 def stream_callback(stream, device, *args, **kwargs):
+    global ws_stream
+    ws_stream.send(stream)
     print("X")
 
 
 def new_robot(robot, *args, **kwargs):
+    global ws_messages
     print(f"New Robot connected ({robot.information.device_name})")
+    message = {"event": "robot_connected", "device_id": robot.information.device_id}
+    ws_messages.send(message)
 
 
 def robot_disconnected(robot, *args, **kwargs):
-    ...
+    global ws_messages
+    print(f"Robot disconnected ({robot.information.device_name})")
+    message = {"event": "robot_disconnected", "device_id": robot.information.device_id}
+    ws_messages.send(message)
 
+def websocket_callback(message):
+    print(f"Received message: {message}")
 
 def run_hardware_manager():
+    global ws_stream
+    global ws_messages
     hardware_manager = HardwareManager()
     hardware_manager.start()
-
+    ws_stream = WebsocketClass('localhost', 8765, start=True)
+    ws_messages = WebsocketClass('localhost', 8766, start=True)
+    ws_messages.set_message_callback(websocket_callback)
     hardware_manager.registerCallback('stream', stream_callback)
     hardware_manager.registerCallback('robot_connected', new_robot)
 
