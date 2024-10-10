29339ef077b0d6aedc647c639aa4a8b640b94698
LionWeber <46690954+LionWeber@users.noreply.github.com>
2024-10-10 20:28:48 +0200

    test

--- scioi_hardware_manager/_archive/server_dummy/run_server_dummy.py
+++ scioi_hardware_manager/_archive/server_dummy/run_server_dummy.py
@@ -1,58 +1,94 @@
 import time
-
-from device_manager.devices.robots.twipr.twipr import TWIPR
+import sys
+import os
+import json
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from hardware_manager.devices.robots.twipr.twipr import TWIPR
+from hardware_manager.helper.websocket_server import WebsocketClass
 from server_dummy.server import HardwareManagerDummy
 from server_dummy.twipr_dummy import TWIPR_Dummy
+import threading
+import random
 
-
+ws = None
 def callback_stream(message, device: TWIPR_Dummy):
-    print(f"STREAM from {device.information.device_id}")
+    global ws
+    #print(f"STREAM from {device.information.device_id}")
+    # Convert message to JSON and send to the websocket
+    id = message.data["general"]["id"]
 
+    message = {id: [message.data]}
+    ws.send(message)
 
 def device_connected_callback(device: TWIPR_Dummy):
+    global ws
     print(f"Device Connected: {device.information.device_id}")
-
+    msg = {'event': 'connected', 'device_id': device.information.device_id}
+    #ws.send(msg)
 
 def device_disconnected_callback(device: TWIPR_Dummy):
+    global ws
     print(f"Device disconnected: {device.information.device_id}")
+    msg = {'event': 'disconnected', 'device_id': device.information.device_id}
+    #ws.send(msg)
+    
+"""
+async def websocket_server():
+    async with websockets.serve(register, "localhost", 8765):
+        await asyncio.Future()  # Run forever
+"""
+def create_robots(max_number_of_robots = 10):
+    robots = []
+    for i in range(max_number_of_robots):
+        robot = TWIPR_Dummy()
+        robot.information.device_id = f"twipr{i+1}"
+        robot.registerCallback('stream', callback_stream)
+        robots.append(robot)
+    return robots
 
+def create_random_robot_connections(min_number_of_robots = 1, max_number_of_robots = 10):
+    # create a list with either 0 or 1 randomly (lenght of list = max_number_of_robots)
+    random_robot_connections = [random.choice([0, 1]) for i in range(max_number_of_robots)]
+    while sum(random_robot_connections) < min_number_of_robots:
+        random_robot_connections[random_robot_connections.index(0)] = 1
+    return random_robot_connections
+
+def connect_disconnect_robots(robots, old_robot_connections, new_robot_connections, server):
+    for i in range(len(robots)):
+        if old_robot_connections[i] == 0 and new_robot_connections[i] == 1:
+            server.addNewDevice(robots[i])
+        elif old_robot_connections[i] == 1 and new_robot_connections[i] == 0:
+            server.removeDevice(robots[i].information.device_id)
+def print_connected_robots(robots_connections):
+    for i,con in enumerate(robots_connections):
+        if con == 1:
+            print("Robot {} is connected".format(i))
+
+def run_robot_loop(server):
+    max_number_of_robots = 10
+    robots = create_robots(max_number_of_robots)
+    old_robot_connections = [0]*max_number_of_robots
+    new_robot_connections = create_random_robot_connections()
+    time_to_change_robots = 30
+    while True:
+        connect_disconnect_robots(robots, old_robot_connections, new_robot_connections,server)
+        old_robot_connections = new_robot_connections
+        new_robot_connections = create_random_robot_connections()
+        print(f"change robot connections:")
+        print_connected_robots(new_robot_connections)
+        print("--------------------")
+        time.sleep(time_to_change_robots)
 
 def main():
+    global ws
     server = HardwareManagerDummy()
     server.registerCallback('new_device', device_connected_callback)
     server.registerCallback('device_disconnected', device_disconnected_callback)
+    ws = WebsocketClass('localhost', 8765, start=True)
+    run_robot_loop(server)
 
-    twipr1 = TWIPR_Dummy()
-    twipr1.information.device_id = 'twipr1'
-    twipr1.information.device_class = 'robot'
-    twipr1.information.device_type = 'twipr'
-    twipr1.information.name = 'TWIPR 1'
-    twipr1.information.address = ''
-    twipr1.information.revision = 3
 
-    twipr1.registerCallback('stream', callback_stream)
-
-    twipr2 = TWIPR_Dummy()
-    twipr2.information.device_id = 'twipr2'
-    twipr2.information.device_class = 'robot'
-    twipr2.information.device_type = 'twipr'
-    twipr2.information.name = 'TWIPR 2'
-    twipr2.information.address = ''
-    twipr2.information.revision = 3
-
-    twipr2.registerCallback('stream', callback_stream)
-
-    server.addNewDevice(twipr1)
-    server.addNewDevice(twipr2)
-
-    time.sleep(5)
-    server.removeDevice('twipr2')
-    time.sleep(2)
-    server.addNewDevice(twipr2)
-
-    while True:
-        time.sleep(1)
-
+    
 
 if __name__ == '__main__':
     main()
--- scioi_hardware_manager/_archive/server_dummy/twipr_dummy.py
+++ scioi_hardware_manager/_archive/server_dummy/twipr_dummy.py
@@ -2,13 +2,18 @@ import copy
 import threading
 import time
 import random
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from hardware_manager.communication.connection import Connection
+from hardware_manager.communication.protocols.protocol import Message
+from hardware_manager.communication.protocols.tcp.tcp_json_protocol import TCP_JSON_Message
+from hardware_manager.devices.robots.twipr.twipr import TWIPR
+#from hardware_manager.devices.robots.twipr.twipr_data import twipr_wifi_sample
+from hardware_manager.devices.robots.twipr.twipr_data import get_twipr_wifi_sample
+import numpy as np
 
-from device_manager.communication.connection import Connection
-from device_manager.communication.protocols.protocol import Message
-from device_manager.communication.protocols.tcp.tcp_json_protocol import TCP_JSON_Message
-from device_manager.devices.robots.twipr.twipr import TWIPR
-from device_manager.devices.robots.twipr.twipr_data import twipr_wifi_sample
-
+mapsize = [3,3]
 
 class ConnectionDummy(Connection):
     registered: bool
@@ -32,10 +37,17 @@ class TWIPR_Dummy(TWIPR):
         self._thread = threading.Thread(target=self._dummy_thread_function, daemon=True)
         self._thread.start()
 
-    def _generateStreamMessage(self):
+    def _generateStreamMessage(self,i=0,positional_offset=[0,0],radius=0.5):
         msg = TCP_JSON_Message()
-
-        data = copy.deepcopy(twipr_wifi_sample)
+        #data = copy.deepcopy(twipr_wifi_sample)
+        name = self.information.name
+        id = self.information.device_id
+        data = get_twipr_wifi_sample(i, name,id, 0.005,radius)
+        data['estimation']['state']['x'] += positional_offset[0]
+        data['estimation']['state']['y'] += positional_offset[1]
+        # cap the x and y values to 0 and mapsize[0] and mapsize[1]
+        data['estimation']['state']['x'] = max(0, min(data['estimation']['state']['x'], mapsize[0]))
+        data['estimation']['state']['y'] = max(0, min(data['estimation']['state']['y'], mapsize[1]))
         msg.data = data
         msg.type = 'stream'
 
@@ -43,7 +55,15 @@ class TWIPR_Dummy(TWIPR):
 
     def _dummy_thread_function(self):
         time.sleep(random.uniform(0.01, 0.1))
+        i = 0
+        positional_offset = np.random.uniform(low=0, high=min(mapsize), size=(2,)).tolist()
+        radius = random.uniform(0.2, 1)
         while True:
             if self.connection.registered:
-                self._generateStreamMessage()
-            time.sleep(1)
+                self._generateStreamMessage(i,positional_offset,radius)
+                i += 1
+            else:
+                i = 0
+                positional_offset = np.random.uniform(low=-5, high=5, size=(2,)).tolist()
+                radius = random.uniform(0.75, 3)
+            time.sleep(0.1)
